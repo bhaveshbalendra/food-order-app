45c2fc2cbfbe7f2fe869f99eabd4602a7055545f
bhaveshbalendra <balendrabhavesh@gmail.com>
2025-06-28 23:50:20 +0530

    COMMIET

--- frontend/src/components/Heading.tsx
+++ frontend/src/components/Heading.tsx
@@ -1,5 +1,10 @@
 const Heading = () => {
   console.log("Header");
+  console.log("Header");
+  console.log("Header");
+  console.log("Header");
+  console.log("Header");
+  console.log("Header");
   return <div>header</div>;
 };
 
