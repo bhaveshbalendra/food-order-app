d435d15cbe040015400c2fde3562d4cf19c3783a
bhaveshbalendra <balendrabhavesh@gmail.com>
2025-08-09 21:55:37 +0530

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 const App = () => {
   console.log("App component rendered");
   console.log("App component rendered");
-  console.log("App component rendered");
 
   return <div>Main App</div>;
 };
