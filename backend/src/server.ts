dca3becc60bafed02db9494d848ef17c611f6858
bhaveshbalendra <balendrabhavesh@gmail.com>
2025-08-26 21:35:46 +0530

    commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const PORT = 8004;
 const app = express();
-
+console.log(app);
 app.use(express.json());
 
 // Start the server with correct parameters
