02abd2250e601bc01f1a0ad3aa0adcde15e42331
bhaveshbalendra <balendrabhavesh@gmail.com>
2025-08-28 21:41:23 +0530

    commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,4 +8,5 @@ app.use(express.json());
 // Start the server with correct parameters
 app.listen(PORT, () => {
   console.log(`Food Order App running on PORT:${PORT}`);
+  console.log(`Food Order App running on PORT:${PORT}`);
 });
